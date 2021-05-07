ab10b185aef4bcff7e1da3a7ad7c0d6d1ee1f88e
SiyuanMa0316 <masiyuan0@gmail.com>
2021-05-07 22:29:20 +0800

    cellSize=8

--- lab2_rework/3_render/cudaRenderer.cu
+++ lab2_rework/3_render/cudaRenderer.cu
@@ -1002,7 +1002,7 @@ CudaRenderer::render() {
      //double timeStart = CycleTimer::currentSeconds();
     
     //printf("%d, %d\n", image->width, image->height);
-    int cellSize=16;
+    int cellSize=8;
     int cellNumX=(image->width+cellSize-1)/cellSize;
     int cellNumY=(image->height+cellSize-1)/cellSize;
     int cellNum = cellNumX*cellNumY;
